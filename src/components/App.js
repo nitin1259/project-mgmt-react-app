23294235d16bb5b8ef541b7f060c8060f0baecf8
Nitin SINGH <nitinkumar.singh@f5.com>
2020-04-11 20:13:41 +0530

    simple routing

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import AboutPage from "./AboutPage";
+import HomePage from "./HomePage";
+import Header from "./common/Header";
+
+function App() {
+  function getPage() {
+    const route = window.location.pathname;
+
+    if (route === "/about") return <AboutPage />;
+    else return <HomePage />;
+  }
+
+  return (
+    <div className="container-fluid">
+      <Header />
+      {getPage()}
+    </div>
+  );
+}
+
+export default App;
