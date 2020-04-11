8167fc5404e31fd9413aff58cc187db9a8b75ca5
Nitin SINGH <nitinkumar.singh@f5.com>
2020-04-11 19:40:21 +0530

    adding the first component

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>Welcome to Project Management React App</h1>
+      <p>This is compose of React, React Router, flux and later redux</p>
+    </div>
+  );
+}
+
+export default HomePage;
