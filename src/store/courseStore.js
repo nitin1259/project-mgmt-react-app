b38af1fc189182935026ec9b92e1ffb8d49c5039
Nitin SINGH <nitinkumar.singh@f5.com>
2020-04-12 16:19:35 +0530

    store: private storage

--- src/store/courseStore.js
+++ src/store/courseStore.js
@@ -1,8 +1,9 @@
 import { EventEmitter } from "events";
 import dispatcher from "../appDispatcher";
+import actionTypes from "../actions/actionTypes";
 
 const CHANGE_EVENT = "change";
-
+let _courses = [];
 class CourseStore extends EventEmitter {
   addChangeListener(callback) {
     this.on(CHANGE_EVENT, callback);
@@ -15,12 +16,26 @@ class CourseStore extends EventEmitter {
   emitChange() {
     this.emit(CHANGE_EVENT);
   }
+
+  getCourses() {
+    return _courses;
+  }
+
+  getCoursesbySlug(slug) {
+    return _courses.find((course) => course.slug === slug);
+  }
 }
 
 const store = new CourseStore();
 
 dispatcher.register((action) => {
-  switch (action) {
+  switch (action.actionType) {
+    case actionTypes.CHANGE_EVENT:
+      _courses.push(action.course);
+      store.emitChange();
+      break;
+    default:
+    // nothing to mention here
   }
 });
 
