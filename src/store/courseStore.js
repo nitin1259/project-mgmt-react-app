5d1f6f46ddc0c8504a464e84449dc932d546dd28
Nitin SINGH <nitinkumar.singh@f5.com>
2020-04-12 15:57:21 +0530

    creating store in flux

---
+++ src/store/courseStore.js
@@ -0,0 +1,19 @@
+import { EventEmitter } from "events";
+
+const CHANGE_EVENT = "change";
+
+class CourseStore extends EventEmitter {
+  addChangeListener(callback) {
+    this.on(CHANGE_EVENT, callback);
+  }
+
+  removeChangeListener(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  }
+
+  emitChange() {
+    this.emit(CHANGE_EVENT);
+  }
+}
+
+export default new CourseStore();
